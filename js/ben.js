c63f68784e8ec6d05ceb19b39c3106611350cad3
Carlos <xtrimgel@gmail.com>
2016-08-30 21:41:25 -0700

    Add files via upload

---
+++ js/ben.js
@@ -0,0 +1,7 @@
+<script language="JavaScript1.1"> 
+// distributed by http://www,hypergurl.com <!-- var debug = true; function 
+right(e) { if (navigator.appName == 'Netscape' && (e.which == 3 || e.which 
+== 2)) return false; else if (navigator.appName == 'Microsoft Internet Explorer' 
+&& (event.button == 2 || event.button == 3)) { alert('This Page is fully 
+protected!'); return false; } return true; } document.onmousedown=right; if (document.layers) 
+window.captureEvents(Event.MOUSEDOWN); window.onmousedown=right; //--></script>
---
+++ js/gen.js
@@ -0,0 +1,7 @@
+<script language="JavaScript"> 
+// distributed by http://hypergurl.com <!-- var popup="Sorry, right-click 
+is disabled.\n\nThis Site Copyright ©2000"; function noway(go) { if 
+(document.all) { if (event.button == 2) { alert(popup); return false; } } if (document.layers) 
+{ if (go.which == 3) { alert(popup); return false; } } } if (document.layers) 
+{ document.captureEvents(Event.MOUSEDOWN); } document.onmousedown=noway; // --> 
+</script>
